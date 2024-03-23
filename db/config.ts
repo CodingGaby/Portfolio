a45404173b500e4acbe8c1b2d971dda12ce5ca27
Gaby Osorio <macma.cube1@gmail.com>
2024-03-23 07:51:42 -0600

    focus

--- db/config.ts
+++ db/config.ts
@@ -1,17 +1,19 @@
 import { defineDb } from 'astro:db';
 import { defineTable, column, NOW } from 'astro:db';
 
-const Proyects = defineTable({
+const Proyect = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
+    name: column.text({ unique: true }),
     desc: column.text(),
-    homePage: column.text()
+    homePage: column.text(),
+    img: column.text()
   },
 });
 
 export default defineDb({
   tables: {
-    Proyects
+    Proyect
   },
 })
 
