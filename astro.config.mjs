adc0512b1e35dc13c37164205e33d9ec60b1a9fb
CodingGaby <macma.cube1@gmail.com>
2024-03-26 14:46:22 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
   integrations: [tailwind(), db()],
+  output: "hybrid",
+  adapter: vercel(),
 });
