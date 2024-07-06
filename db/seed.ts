f01a30c21039a3268974c204e9e42e2ad0086519
CodingGaby <macma.cube1@gmail.com>
2024-07-05 21:17:04 -0600

    Proyects Getting Better

--- db/seed.ts
+++ db/seed.ts
@@ -6,22 +6,15 @@ export default async function() {
       { 
         id: crypto.randomUUID(),
         name: "SpaceXLaunches",
-        desc: "Aplicación en Astro ocupando la api \"SpaceXdata\" para mostrar los lanzamientos de SpaceX",
+        desc: "An application built with AstroJS that uses the SpaceXdata API to display the latest SpaceX launches.",
         homePage: "https://spacexlaunches-b.vercel.app/",
         img: "https://firebasestorage.googleapis.com/v0/b/imgspor.appspot.com/o/spacex.png?alt=media&token=2e200b65-f9c6-4cf3-963c-4bcf6d2e6b01"
       },
       {
         id: crypto.randomUUID(),
         name: "SnakeJS",
-        desc: "Juego Snake creado con JavaScript",
+        desc: "A modern take on the classic Snake game, built with JavaScript, HTML, and CSS. The game features responsive design.",
         homePage: "https://jsnake.vercel.app/",
-        img: "https://firebasestorage.googleapis.com/v0/b/imgspor.appspot.com/o/Screenshot%202024-03-26%20123829.png?alt=media&token=ca233e29-6c6a-4bc8-8321-6f8f4ffa9148"
-      },
-      {
-        id: crypto.randomUUID(),
-        name: "Caira",
-        desc: "Proyecto de HackTec-Puebla, usando tecnologias de AstroJS, Caira es una red social académica enfocada en la distribucion de articulos de investigacion para socios de Ellucian",
-        homePage: "https://caira.vercel.app/",
         img: "https://firebasestorage.googleapis.com/v0/b/imgspor.appspot.com/o/Screenshot%202024-03-26%20123829.png?alt=media&token=ca233e29-6c6a-4bc8-8321-6f8f4ffa9148"
       },
     ]
